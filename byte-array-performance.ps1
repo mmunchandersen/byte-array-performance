69a6fcba6d52a41304643a7ecf5bbf6f184133f0
Morten Munch-Andersen <mmunchandersen@gmail.com>
2022-06-05 09:14:04 +0200

    Update byte-array-performance.ps1

--- byte-array-performance.ps1
+++ byte-array-performance.ps1
@@ -7,42 +7,48 @@ Clear-Host
 
 $pictures = Get-ChildItem -Path "$picturesPath\*" -Include *.jpg
 
-Write-Output "Running $numberOfTestRuns test runs on $($pictures.Count) pictures"
+if ($pictures.Count -eq 0) {
+    Write-Output "No jpg pictures in $picturesPath"
+} else {
 
-for ($i = 1; $i -le $numberOfTestRuns; $i++) {
+    Write-Output "Running $numberOfTestRuns test runs on $($pictures.Count) picture(s)"
 
-    $GetContentResult = Measure-Command -Expression {
-        foreach ($picture in $pictures) {
-            [byte[]]$bytes1 = Get-Content $picture.FullName -Encoding byte
-        }
-    }
+    for ($i = 1; $i -le $numberOfTestRuns; $i++) {
 
-    $GetContentRawResult = Measure-Command -Expression {
-        foreach ($picture in $pictures) {
-            [byte[]]$bytes3 = Get-Content $picture.FullName -Raw -Encoding byte
-        }
-    }
-
-    $SystemIOStreamResults = Measure-Command -Expression {
-        foreach ($picture in $pictures) {
-            [System.IO.Stream]$Stream = [System.IO.File]::OpenRead($picture.FullName)
-            try {
-                [byte[]]$bytes2 = New-Object byte[] $Stream.length
-                [void] $Stream.Read($bytes2, 0, $Stream.Length)
-            } finally {
-                $Stream.Close()
+        $GetContentResult = Measure-Command -Expression {
+            foreach ($picture in $pictures) {
+                [byte[]]$bytes1 = Get-Content $picture.FullName -Encoding byte
             }
         }
+
+        $GetContentRawResult = Measure-Command -Expression {
+            foreach ($picture in $pictures) {
+                [byte[]]$bytes3 = Get-Content $picture.FullName -Raw -Encoding byte
+            }
+        }
+
+        $SystemIOStreamResults = Measure-Command -Expression {
+            foreach ($picture in $pictures) {
+                [System.IO.Stream]$Stream = [System.IO.File]::OpenRead($picture.FullName)
+                try {
+                    [byte[]]$bytes2 = New-Object byte[] $Stream.length
+                    [void] $Stream.Read($bytes2, 0, $Stream.Length)
+                } finally {
+                    $Stream.Close()
+                }
+            }
+        }
+
+        $output = [PSCustomObject][ordered]@{
+            Iteration      = "Test $i"
+            GetContent     = $GetContentResult.TotalSeconds
+            GetContentRaw  = $GetContentRawResult.TotalSeconds
+            SystemIoStream = $SystemIOStreamResults.TotalSeconds
+        }
+
+        Write-Output $output
+
     }
-
-    $output = [PSCustomObject][ordered]@{
-        Iteration      = "Test $i"
-        GetContent     = $GetContentResult.TotalSeconds
-        GetContentRaw  = $GetContentRawResult.TotalSeconds
-        SystemIoStream = $SystemIOStreamResults.TotalSeconds
-    }
-
-    Write-Output $output
-
 }
+
 # inspiration: https://jeffbrown.tech/measuring-performance-in-powershell/
