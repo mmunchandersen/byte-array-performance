6002b318f2b038d51ad53a8a468ddda6a4031f38
Morten Munch-Andersen <mmunchandersen@gmail.com>
2022-06-05 09:44:48 +0200

    Update byte-array-performance.ps1

--- byte-array-performance.ps1
+++ byte-array-performance.ps1
@@ -4,37 +4,66 @@ $numberOfTestRuns = 3
 
 Clear-Host
 
-
+$psVersion = $PSVersionTable.PSVersion
 $pictures = Get-ChildItem -Path "$picturesPath\*" -Include *.jpg
 
 if ($pictures.Count -eq 0) {
     Write-Output "No jpg pictures in $picturesPath"
 } else {
 
-    Write-Output "Running $numberOfTestRuns test runs on $($pictures.Count) picture(s)"
+    Write-Output "Running $numberOfTestRuns test runs on $($pictures.Count) picture(s). PS version: $($psVersion.Major)"
 
     for ($i = 1; $i -le $numberOfTestRuns; $i++) {
 
-        $GetContentResult = Measure-Command -Expression {
-            foreach ($picture in $pictures) {
-                [byte[]]$bytes1 = Get-Content $picture.FullName -Encoding byte
-            }
-        }
+        if ($psVersion.Major -eq 5) {
 
-        $GetContentRawResult = Measure-Command -Expression {
-            foreach ($picture in $pictures) {
-                [byte[]]$bytes3 = Get-Content $picture.FullName -Raw -Encoding byte
+            $GetContentResult = Measure-Command -Expression {
+                foreach ($picture in $pictures) {
+                    [byte[]]$bytes1 = Get-Content $picture.FullName -Encoding byte
+                }
             }
-        }
 
-        $SystemIOStreamResults = Measure-Command -Expression {
-            foreach ($picture in $pictures) {
-                [System.IO.Stream]$Stream = [System.IO.File]::OpenRead($picture.FullName)
-                try {
-                    [byte[]]$bytes2 = New-Object byte[] $Stream.length
-                    [void] $Stream.Read($bytes2, 0, $Stream.Length)
-                } finally {
-                    $Stream.Close()
+            $GetContentRawResult = Measure-Command -Expression {
+                foreach ($picture in $pictures) {
+                    [byte[]]$bytes3 = Get-Content $picture.FullName -Raw -Encoding byte
+                }
+            }
+
+            $SystemIOStreamResults = Measure-Command -Expression {
+                foreach ($picture in $pictures) {
+                    [System.IO.Stream]$Stream = [System.IO.File]::OpenRead($picture.FullName)
+                    try {
+                        [byte[]]$bytes2 = New-Object byte[] $Stream.length
+                        [void] $Stream.Read($bytes2, 0, $Stream.Length)
+                    } finally {
+                        $Stream.Close()
+                    }
+                }
+            }
+
+        } elseif ($psVersion.Major -eq 7) {
+
+            $GetContentResult = Measure-Command -Expression {
+                foreach ($picture in $pictures) {
+                    [byte[]]$bytes1 = Get-Content $picture.FullName -AsByteStream
+                }
+            }
+
+            $GetContentRawResult = Measure-Command -Expression {
+                foreach ($picture in $pictures) {
+                    [byte[]]$bytes3 = Get-Content $picture.FullName -Raw -AsByteStream
+                }
+            }
+
+            $SystemIOStreamResults = Measure-Command -Expression {
+                foreach ($picture in $pictures) {
+                    [System.IO.Stream]$Stream = [System.IO.File]::OpenRead($picture.FullName)
+                    try {
+                        [byte[]]$bytes2 = New-Object byte[] $Stream.length
+                        [void] $Stream.Read($bytes2, 0, $Stream.Length)
+                    } finally {
+                        $Stream.Close()
+                    }
                 }
             }
         }
